0cc34d8cbd1e4285de39e2231d44eb1892879462
FabiTheGuy <fabian.doeller@mailfence.com>
2024-01-19 18:12:18 +0100

    edit: replaced window-flags loading with for instead of while

--- include/renderer.h
+++ include/renderer.h
@@ -1,12 +1,17 @@
+// TODO: Write comment for this Header
 #ifndef C_PONG_RENDERER_H
 #define C_PONG_RENDERER_H
 
 #include <raylib.h>
 
 #include "window.h"
+#include "entities.h"
 
-
+// TODO: Write comment for this function
 void init_renderer(Window* window);
 
+void draw_player(Window* window, Player* player);
+
+void draw_ball(Window* window, Player* player);
 
 #endif /* C_PONG_RENDERER_H */
