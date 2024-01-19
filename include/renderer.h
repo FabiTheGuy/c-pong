e2dee0b75221b0d7c5a4152360d5ddc6d77a318e
FabiTheGuy <fabian.doeller@mailfence.com>
2024-01-19 18:30:03 +0100

    edit: added draw functions for entities and comments

--- include/renderer.h
+++ include/renderer.h
@@ -1,4 +1,10 @@
-// TODO: Write comment for this Header
+/**
+ * @file renderer.h
+ * @author FabiTheGuy (fabian.doeller@mailfence.com)
+ * @brief Contains all function which are neccessary to draw to the screen
+ * @version 0.1
+ * @date 2024-01-19
+ */
 #ifndef C_PONG_RENDERER_H
 #define C_PONG_RENDERER_H
 
@@ -7,11 +13,28 @@
 #include "window.h"
 #include "entities.h"
 
-// TODO: Write comment for this function
+/**
+ * @brief Initializes the Renderer by creating the Window an which the renderer
+ * will draw
+ * 
+ * @param window Window instance 
+ */
 void init_renderer(Window* window);
 
+/**
+ * @brief Draws the Player according to the window scaling to the monitor
+ * 
+ * @param window Window instance
+ * @param player Player instance
+ */
 void draw_player(Window* window, Player* player);
 
-void draw_ball(Window* window, Player* player);
+/**
+ * @brief Draws the Ball according ti the window scaling to the monitor
+ * 
+ * @param window 
+ * @param ball 
+ */
+void draw_ball(Window* window, Ball* ball);
 
 #endif /* C_PONG_RENDERER_H */
--- src/renderer.c
+++ src/renderer.c
@@ -7,9 +7,23 @@
 
 void init_renderer(Window* window)
 {
-  size_t index = 0;
-
-  while (window->flags[index] != '\0')
-    SetConfigFlags(window->flags[index++]);
+  for (size_t i = 0; window->flags[i] != '\0'; i++)
+    SetConfigFlags(window->flags[i]);
   InitWindow(window->size.x, window->size.y, window->title);
+}
+
+void draw_player(Window* window, Player* player)
+{
+  Vector2 scaled_position = (Vector2) { player->position.x * window->scale.x, player->position.y * window->scale.y };
+  Vector2 scaled_size = (Vector2) { player->size.x * window->scale.x, player->size.y * window->scale.y };
+
+  DrawRectangleV(scaled_position, scaled_size, RAYWHITE);
+}
+
+void draw_ball(Window* window, Ball* ball)
+{
+  Vector2 scaled_position = (Vector2) { ball->position.x * window->scale.x, ball->position.y * window->scale.y };
+  float scaled_radius = ball->radius * window-> scale.x;
+
+  DrawCircleV(scaled_position, scaled_radius, RAYWHITE);
 }
