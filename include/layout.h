279243b762e2dea74bfae0ae6a7e32651141bfbb
FabiTheGuy <fabian.doeller@mailfence.com>
2025-05-31 18:05:06 +0200

    feat: add percentage layout macros

---
+++ include/layout.h
@@ -0,0 +1,10 @@
+#ifndef C_PONG_LAYOUT_H
+#define C_PONG_LAYOUT_H
+
+#include "config.h"
+
+
+#define WIN_PERC_X(percentage) percentage * winDimensions.x / 100 
+#define WIN_PERC_Y(percentage) percentage * winDimensions.y / 100 
+
+#endif /* C_PONG_LAYOUT_H */
